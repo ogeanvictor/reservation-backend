108d56875f9ee9273b6d4f732970943f5bdd5e2d
Gean <geanvictorduque@gmail.com>
2025-05-29 23:11:55 -0300

    feat: create auth guard routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,11 +3,13 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 import { AuthLoginDto } from './dtos/auth-login.dto';
+import { Public } from 'src/common/interceptors/auth.metadata';
 
 @Controller('auth')
 export class AuthController {
   constructor(private service: AuthService) {}
 
+  @Public()
   @Post('login')
   async login(@Body() body: AuthLoginDto) {
     return await this.service.login(body);
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
 
 import { UserModule } from '../user/user.module';
 
+import { jwtConstanst } from './constants';
+
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstanst.secret,
+      signOptions: { expiresIn: '10h' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AuthModule {}
