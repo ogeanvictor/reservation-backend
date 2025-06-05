ff03d4cbb0d0b26c9add96d9721407372e29654d
Gean <geanvictorduque@gmail.com>
2025-06-04 22:58:09 -0300

    feat: install swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Reservation Backend')
+    .setDescription('The reservation backend API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
